86114db413e4e54663927fff5e0cb2607a50c472
Vinceline Bertrand <vbertrand2023@LIBLTM-LP7RWD.local>
2024-08-25 19:58:18 -0400

    first commit

---
+++ lab-task-squirrel/PhotoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoViewController.swift
+//  lab-task-squirrel
+//
+//  Created by Vinceline Bertrand on 8/25/24.
+//
+
+import UIKit
+ 
+ class PhotoViewController: UIViewController {
+     @IBOutlet weak var photoView: UIImageView!
+         
+         var task: Task!
+         
+         override func viewDidLoad() {
+             super.viewDidLoad()
+             photoView.image = task.image
+         }
+ }
